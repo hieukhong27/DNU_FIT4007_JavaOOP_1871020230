ebb1ad1d2833fe32769c82afae1e6f08062a7ec2
hieukhong27 <hieuhieu27306@gmail.com>
2025-10-21 20:00:45 +0700

    Update new feature

---
+++ src/part1/basic/variables/Example09NextCharacter.java
@@ -0,0 +1,18 @@
+package part1.basic.variables;
+
+import java.util.Scanner;
+
+public class Example09NextCharacter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập chữ cái thường từ a đến z: ");
+        char H = sc.next().charAt(0);
+
+        if (H >= 'a' && H <= 'z') {
+            char N = (char) (H + 1);
+
+            System.out.print("Chữ cái liền sau của " + H + " là: " + N);
+            return;
+        }
+    }
+}
