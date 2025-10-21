fe25dd910bcb147d209866611320ae3d9343e59e
hieukhong27 <hieuhieu27306@gmail.com>
2025-10-21 21:15:56 +0700

    Update new feature

--- src/part1/basic/variables/Example11SwapValues.java
+++ src/part1/basic/variables/Example11SwapValues.java
@@ -5,6 +5,14 @@ import java.util.Scanner;
 public class Example11SwapValues {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+        System.out.print("Nhập giá trị a: ");
+        int a = sc.nextInt();
+        System.out.print("Nhập giá trị b: ");
+        int b = sc.nextInt();
+
+        int c = (int)a;
+        System.out.print("Gía trị a là: " + b + "\n");
+        System.out.print("Gía trị b là: " + c);
+
     }
 }
