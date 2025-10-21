c6264a353ffb0c7e15cc6cbcd236e5b20240a592
hieukhong27 <hieuhieu27306@gmail.com>
2025-10-21 12:14:45 +0700

    Add missing src file

--- src/part1/basic/variables/Example06FindTheLastDigit.java
+++ src/part1/basic/variables/Example06FindTheLastDigit.java
@@ -1,4 +1,14 @@
 package part1.basic.variables;
 
+import java.util.Scanner;
+
 public class Example06FindTheLastDigit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập số nguyên: ");
+        int N = sc.nextInt();
+
+        int TheLastDigit = Math.abs(N % 10);
+        System.out.print("Chữ số cuối cùng của số nguyên là: " + TheLastDigit);
+    }
 }
