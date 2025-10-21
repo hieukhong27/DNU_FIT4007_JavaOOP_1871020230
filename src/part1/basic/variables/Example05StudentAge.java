eae09b9339e593f3f46f5b7c5a25501ed0bac293
hieukhong27 <hieuhieu27306@gmail.com>
2025-10-21 11:18:16 +0700

    Add missing src file

--- src/part1/basic/variables/Example05StudentAge.java
+++ src/part1/basic/variables/Example05StudentAge.java
@@ -1,5 +1,15 @@
 package part1.basic.variables;
+import java.util.Scanner;
 
 public class Example05StudentAge {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Nhập số tổi của sinh viên: ");
+        int age = sc.nextInt();
+
+        int yearofbirth = 2023 - age;
+
+        System.out.print("Năm sinh của sinh viên: " + yearofbirth);
+    }
 }
