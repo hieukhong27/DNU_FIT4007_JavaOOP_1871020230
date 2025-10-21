12077bc2583004e4871ce6512e5b15611f4cfd07
hieukhong27 <hieuhieu27306@gmail.com>
2025-10-21 21:08:26 +0700

    Update new feature

---
+++ src/part1/basic/variables/Example11SwapValues.java
@@ -0,0 +1,10 @@
+package part1.basic.variables;
+
+import java.util.Scanner;
+
+public class Example11SwapValues {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+    }
+}
