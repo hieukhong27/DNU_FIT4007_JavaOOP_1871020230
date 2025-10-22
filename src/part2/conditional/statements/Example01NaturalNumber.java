19b25bfdcf1d446fb749a155fe906ec013422dbd
hieukhong27 <hieuhieu27306@gmail.com>
2025-10-22 10:21:11 +0700

    Update new feature

--- src/part2/conditional/statements/Example01NaturalNumber.java
+++ src/part2/conditional/statements/Example01NaturalNumber.java
@@ -1,5 +1,19 @@
 package part2.conditional.statements;
 
+import java.util.Scanner;
+
 public class Example01NaturalNumber {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("--- Bài toán kiểm tra số tự nhiên ---\n");
+        System.out.print("Nhập một số nguyên bất kì: ");
+        int a = sc.nextInt();
+
+        if (a < 0){
+            System.out.print("Số" + a + " là số nguyên âm. Không phải số tự nhiên!!");
+        }
+        else {
+            System.out.print("Số" + a + " là số tự nhiên.");
+        }
+    }
 }
