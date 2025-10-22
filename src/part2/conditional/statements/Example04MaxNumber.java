ba86805a8332cf983e5dcdd6bdf7187e43ab0f18
hieukhong27 <hieuhieu27306@gmail.com>
2025-10-22 15:43:18 +0700

    Update new feature

---
+++ src/part2/conditional/statements/Example04MaxNumber.java
@@ -0,0 +1,23 @@
+package part2.conditional.statements;
+
+import java.util.Scanner;
+
+public class Example04MaxNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập số nguyên a: ");
+        int a = sc.nextInt();
+        System.out.print("Nhập số nguyên b: ");
+        int b = sc.nextInt();
+
+        if(a > b){
+            System.out.print(a + " lớn hơn" + b);
+        }
+        else if(b > a){
+            System.out.print(b + " lớn hơn "+ a);
+        }
+        else if(a == b){
+            System.out.print("H");
+        }
+    }
+}
