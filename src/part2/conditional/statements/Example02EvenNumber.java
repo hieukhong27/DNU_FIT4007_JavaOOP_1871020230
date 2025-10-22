e206fddadadbc690250553b2d476563a9663c65b
hieukhong27 <hieuhieu27306@gmail.com>
2025-10-22 13:30:21 +0700

    Update new feature

---
+++ src/part2/conditional/statements/Example02EvenNumber.java
@@ -0,0 +1,18 @@
+package part2.conditional.statements;
+
+import java.util.Scanner;
+
+public class Example02EvenNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập số nguyên a: ");
+        int a = sc.nextInt();
+
+        if (a % 2 == 0){
+            System.out.print("số chẵn");
+        }
+        else if (a % 2 == 1){
+            System.out.print("Số lẻ");
+        }
+    }
+}
