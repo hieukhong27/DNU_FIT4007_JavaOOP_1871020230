f0165a43faf2f0f88d0f0e67252d2db5d47136e4
hieukhong27 <hieuhieu27306@gmail.com>
2025-10-22 20:28:38 +0700

    Update new feature

--- src/part2/conditional/statements/Example08IntergerCheck.java
+++ src/part2/conditional/statements/Example08IntergerCheck.java
@@ -5,6 +5,17 @@ import java.util.Scanner;
 public class Example08IntergerCheck {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+        System.out.print("--- KIỂM TRA SỐ THỰC CÓ PHẢI SỐ NGUYÊN HAY KHÔNG ---");
+        System.out.print("Nhập một số thực bất kì: ");
+        double f = sc.nextDouble();
+
+        int n = (int) f;
+
+        if (n == f) {
+            System.out.print("Số vừa nhập là số nguyên");
+        }
+        else {
+            System.out.print("Số vừa nhập không phải số nguyên");
+        }
     }
 }
