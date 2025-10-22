74648c16cbe4c3b9015a68a061e584722b190bca
hieukhong27 <hieuhieu27306@gmail.com>
2025-10-22 20:25:32 +0700

    Update new feature

---
+++ src/part2/conditional/statements/Example08IntergerCheck.java
@@ -0,0 +1,10 @@
+package part2.conditional.statements;
+
+import java.util.Scanner;
+
+public class Example08IntergerCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+    }
+}
