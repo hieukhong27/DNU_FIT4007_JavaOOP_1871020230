7000f81801223c9d5446d81a14d17809d98d3a68
hieukhong27 <hieuhieu27306@gmail.com>
2025-10-23 08:54:13 +0700

    Update new feature

---
+++ src/part2/conditional/statements/Example09SoChinhPhuong.java
@@ -0,0 +1,20 @@
+package part2.conditional.statements;
+
+import java.util.Scanner;
+
+public class Example09SoChinhPhuong {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập một số nguyên dương N bất kì: ");
+        int n = sc.nextInt();
+
+        if (n > 0) {
+
+            int c = (int) n;
+
+            if (c == n) {
+                
+            }
+        }
+    }
+}
